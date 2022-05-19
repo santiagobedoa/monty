eb6efdc17c08b340a8bdd3b967903189630b830c
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-19 09:42:08 -0500

    Checker 4: v1

---
+++ add.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * add - function that adds the top two elements of a stack
+ * @stack: element at the top of the stack (head)
+ * @line_number: line number of the command in the file .m
+ *
+ * Return: void
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->prev == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		error_signal = 1;
+	}
+	(*stack)->prev->n = (*stack)->n + (*stack)->prev->n;
+	pop(stack, line_number);
+}
