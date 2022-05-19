6497280b40de79a1ccc66ecad91c34d002069daa
Santiago Bedoya <santiagobedoa@gmail.com>
2022-05-19 13:27:28 -0500

    Checker 10: v4

--- bytecodes/comments.m
+++ bytecodes/comments.m
@@ -3,4 +3,5 @@ push 2
 push 3
    # push 4
 #push hola
+p#sh 4
 pall
--- interpreter.c
+++ interpreter.c
@@ -27,7 +27,7 @@ void interpreter(char *argv[])
 		if (error_signal == 1)
 			break;
 		args = split_line(line);
-		if (args[0] == NULL || strchr(args[0], '#') != NULL)
+		if (args[0] == NULL || args[0][0] == '#')
 		{
 			line_number++;
 			continue;
